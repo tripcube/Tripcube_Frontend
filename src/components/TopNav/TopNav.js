007b96c4e8581adf46685df42658069e3e433258
graS2e <enehyejjang@gmail.com>
2023-09-14 18:06:11 +0900

    로그인 작은 에러 수정-css, 하단바 추가, 상단바 만드는 중

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { BottomNavStyle } from './style';
+import { TopNavStyle } from './style';
 import { Outlet } from 'react-router';
 
-const BottomNav = () => {
+const TopNav = ({ numMenu, name1, url1, name2, url2, name3, url3 }) => {
   const [activeNav, setActiveNav] = useState(1);
   const location = useLocation();
 
@@ -30,42 +30,42 @@ const BottomNav = () => {
 
   return (
     <>
-      <BottomNavStyle>
+      <TopNavStyle>
         <Link to='/home' onClick={() => setActiveNav(1)}>
           {activeNav === 1 ? (
-            <img src='images/nav_home_filled.svg' alt='filled_main_icon' />
+            <img src='images/home_filled.svg' alt='filled_main_icon' />
           ) : (
-            <img src='images/nav_home.svg' alt='main_icon' />
+            <img src='images/home_main.svg' alt='main_icon' />
           )}
         </Link>
         <Link to='/map' onClick={() => setActiveNav(2)}>
           {activeNav === 2 ? (
-            <img src='images/nav_map_filled.svg' alt='filled_map_icon' />
+            <img src='images/map_filled.svg' alt='filled_map_icon' />
           ) : (
-            <img src='images/nav_map.svg' alt='map_icon' />
+            <img src='images/map_locker.svg' alt='map_icon' />
           )}
         </Link>
         <Link to='/scrap' onClick={() => setActiveNav(3)}>
           {activeNav === 3 ? (
-            <img src='images/nav_scrap_filled.svg' alt='filled_scrap_icon' />
+            <img src='images/scrap_filled.svg' alt='filled_scrap_icon' />
           ) : (
-            <img src='images/nav_scrap.svg' alt='scrap_icon' />
+            <img src='images/scrap.svg' alt='scrap_icon' />
           )}
         </Link>
         <Link to='/mypage' onClick={() => setActiveNav(4)}>
           {activeNav === 4 ? (
-            <img src='images/nav_mypage_filled.svg' alt='filled_mypage_icon' />
+            <img src='images/mypage_filled.svg' alt='filled_mypage_icon' />
           ) : (
             <img
-              src={process.env.PUBLIC_URL + 'images/nav_mypage.svg'}
+              src={process.env.PUBLIC_URL + 'images/mypage.svg'}
               alt='mypage_icon'
             />
           )}
         </Link>
-      </BottomNavStyle>
+      </TopNavStyle>
       <Outlet />
     </>
   );
 };
 
-export default BottomNav;
+export default TopNav;
---
+++ src/pages/scrap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scrap = () => {
+  return (
+    <div>
+      <h1>Scrap</h1>
+    </div>
+  );
+};
+
+export default Scrap;
