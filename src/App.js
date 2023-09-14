9a7fb1d7edb96d27f51c3164dd70fd73c8edb0c5
Byungook <43869051+ByungOok@users.noreply.github.com>
2023-09-14 13:35:52 +0900

    .]

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { Reset } from 'styled-reset';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
-import NotFound from './pages/NotFound';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-
-const Container = styled.div`
-  max-width: 430px;
-  height: 932px;
-  margin: 0 auto;
-`;
 
 function App() {
   return (
-    <Container>
-      <Reset />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' index element={<Login />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
