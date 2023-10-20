30d3e3be24ec3bb915f99a72cb2539dcd4ce139f
graS2e <enehyejjang@gmail.com>
2023-10-20 22:42:13 +0900

    스크랩 화면 다 채우기

--- src/pages/Scrap.js
+++ src/pages/Scrap.js
@@ -74,7 +74,7 @@ const MainContainer = (props) => {
 
   return (
     <>
-      <ScrapStyle style={{ minHeight: '270px' }}>
+      <ScrapStyle style={{ minHeight: '100vh' }}>
         {loading1 ? (
           <LinearProgress />
         ) : (
